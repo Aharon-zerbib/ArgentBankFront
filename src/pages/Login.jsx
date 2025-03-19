dc3f52c93df54b5d9e4e9c78216152d616e63f49
Aharon-zerbib <amzyt770@gmail.com>
2025-03-19 14:26:48 +0100

    feat: -ajout de font family -rajout de transaction dans jsson -ajout du ficher Containeraccount qui affichera les transaction dans le Profil -ajout du ficher Login -ajout du Profil ( dans les ficher citer je nest pas encore fait les condition des appelle ...) -ajout de scss sur les ficher mentioner

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,37 @@
+import NavBar from "../components/Nav";
+import "../scss/Login.scss";
+import { NavLink } from "react-router-dom";
+
 const Login = () => {
-  return <h1>Bienvenue sur la page login</h1>;
+  return (
+    <>
+      <NavBar />
+      <div>
+        <main className="main bg-dark">
+          <section className="sign-in-content">
+            <i className="fa fa-user-circle"></i>
+            <h1>Sign In</h1>
+
+            <div className="input-wrapper">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" />
+            </div>
+            <div className="input-wrapper">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" />
+            </div>
+            <div className="input-remember">
+              <input type="checkbox" id="remember-me" />
+              <label htmlFor="remember-me">Remember me</label>
+            </div>
+            <NavLink to="/profil" className="sign-in-button">
+              Sign In
+            </NavLink>
+          </section>
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default Login;
--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -1,5 +1,65 @@
+import { useState } from "react";
+import NavBar from "../components/Nav";
+import Containeraccount from "../components/Containeraccount";
+import "../scss/Profil.scss";
+
 const Profil = () => {
-  return <h1>Bienvenue sur la page profil</h1>;
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleCancelClick = () => {
+    setIsEditing(false);
+  };
+
+  return (
+    <>
+      <NavBar />
+      <main className="main bg-dark">
+        <div className="header-account">
+          {isEditing ? (
+            <>
+              <h1>Edit User info</h1>
+              <div className="container-input">
+                <div>
+                  <label>Username: </label>
+                  <input type="text" />
+                </div>
+                <div>
+                  <label>First Name: </label>
+                  <input type="text" />
+                </div>
+                <div>
+                  <label>Last Name: </label>
+                  <input type="text" />
+                </div>
+              </div>
+              <div className="container-button">
+                <button type="submit">Save</button>
+                <button type="button" onClick={handleCancelClick}>
+                  Cancel
+                </button>
+              </div>
+            </>
+          ) : (
+            <>
+              <h1>
+                Welcome back
+                <br />
+              </h1>
+              <button className="edit-button" onClick={handleEditClick}>
+                Edit Name
+              </button>
+            </>
+          )}
+        </div>
+        <h2 className="sr-only">Accounts</h2>
+        <Containeraccount />
+      </main>
+    </>
+  );
 };
 
 export default Profil;
